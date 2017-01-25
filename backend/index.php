664c4c019d1785d899fdb64a541934e5e7a2e42d
Julius Kiano <julius.kiano@creativeyr.co.ke>
2017-01-25 16:25:06 +0300

    minor refactor

--- backend/index.php
+++ backend/index.php
@@ -14,20 +14,22 @@ $industry = null;
 </form>
 
 <ul>
-    <?php if(!isset($industriesArray["error"]) && !isset($sectorsArray["error"])) {?>
+    <?php if(!isset($industriesArray["error"]) && !isset($sectorsArray["error"])): ?>
         <!-- this can be expanded to make sure that reports actually exist for that sector and industry by looping through report'sector id -->
-        <?php foreach ($industriesArray as $industry) { ?>
+        <?php foreach ($industriesArray as $industry): ?>
             <li><?php echo $industry["industry_name"]; ?>
                 <ul>
-                    <?php foreach ($sectorsArray as $sector) { if ($sector["industry_id"] == $industry["industry_id"]) { ?>
-                        <li><a href="reports.php?sector=<?php echo $sector["sector_id"] ?>"><?php echo $sector["sector_name"];?></a></li>
-            		<?php } } ?>
+                    <?php foreach ($sectorsArray as $sector): ?> 
+                        <?php if ($sector["industry_id"] == $industry["industry_id"]): ?>
+                            <li><a href="reports.php?sector=<?php echo $sector["sector_id"] ?>"><?php echo $sector["sector_name"];?></a></li>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
             	</ul>
             </li>
-        <?php } ?>
-    <?php } else{ ?>
+        <?php endforeach; ?>
+    <?php else: ?>
         <?php echo $industriesArray["error"]; ?>
-    <?php } ?>
+    <?php endif; ?>
 </ul>
 
 <?php include("footer.php"); ?>
